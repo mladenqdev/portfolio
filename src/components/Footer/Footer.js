62819444c087bbc9adfbcee169c99c510f9a8254
Mladen Savic <mladenqdev@gmail.com>
2021-08-30 21:50:27 +0200

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+const Footer = () => (
+    <footer className="footer">
+        <a
+            href="https://github.com/mladenqdev?tab=repositories"
+            className="link footer__link"
+        >
+            Created by Mladen Savic
+        </a>
+    </footer>
+);
+
+export default Footer;
---
+++ src/portfolio.js
@@ -0,0 +1,62 @@
+const header = {
+    homepage: 'https://github.com/mladenqdev?tab=repositories',
+    title: 'MS.',
+};
+
+const about = {
+    name: 'Mladen Savic',
+    role: 'Front End Developer',
+    description:
+        'Professor of English Language and Literature turned Front-end Developer. Quick learner. Highly driven and motivated. Continue scrolling, if you want to find out how a couple of mini projects, that were created for the sole purpose of self-promotion, have totally changed the world!',
+    social: {
+        linkedin: 'https://www.linkedin.com/in/savicmladen/',
+        github: 'https://github.com/mladenqdev?tab=repositories',
+    },
+};
+
+const projects = [
+    {
+        name: 'Project 1',
+        description:
+            'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+        stack: ['SASS', 'TypeScript', 'React'],
+        sourceCode: 'https://github.com',
+        livePreview: 'https://github.com',
+    },
+    {
+        name: 'Project 2',
+        description:
+            'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+        stack: ['SASS', 'TypeScript', 'React'],
+        sourceCode: 'https://github.com',
+        livePreview: 'https://github.com',
+    },
+    {
+        name: 'Project 3',
+        description:
+            'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+        stack: ['SASS', 'TypeScript', 'React'],
+        sourceCode: 'https://github.com',
+        livePreview: 'https://github.com',
+    },
+];
+
+const skills = [
+    'HTML',
+    'CSS',
+    'Sass',
+    'JavaScript',
+    'Handlebars.js',
+    'React.js',
+    'Redux',
+    'Vue.js',
+    'Material UI',
+    'Git',
+    'Adobe Photoshop',
+];
+
+const contact = {
+    email: 'mladjos@msn.com',
+};
+
+export { header, about, projects, skills, contact };
